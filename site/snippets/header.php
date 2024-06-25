e3e5de10dff591dbff8c40c0e9aa4c7783ca6604
stuff <stuff@stuffdev.de>
2024-06-25 22:44:06 +0200

    noCaching Strategy for Development

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -23,6 +23,9 @@
   */
   ?>
   <meta name="robots" content="noindex">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
   <?php
