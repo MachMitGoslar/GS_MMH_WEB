0bd34c28bbf7097c8f00a3c4789d351b2dff4a36
stuff <stuff@stuffdev.de>
2026-02-10 16:15:04 +0100

    fixed oveda events on home page

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -1,11 +1,13 @@
 <?php
 
+use Kirby\Http\Remote;
+
 return function () {
     //Fetches events from oveda for organization and from current date
-    //$json = Remote::get('https://oveda.de/api/v1/event-dates/search?per_page=6&date_from='. date('Y-m-d') .'&organization_id=19')->json();
-    //$events = $json['items'];
+    $json = Remote::get('https://oveda.de/api/v1/event-dates/search?per_page=6&date_from='. date('Y-m-d') .'&organization_id=19')->json();
+    $events = $json['items'];
 
-    $events = [];
+    //$events = [];
 
     return compact(['events']);
 };
--- site/snippets/components/events/eventsListItem.php
+++ site/snippets/components/events/eventsListItem.php
@@ -1,14 +1,19 @@
 <?php
+use Kirby\Toolkit\Str;
 /**
 * @var \Kirby\Cms\Site $site
 * @var \Kirby\Cms\Page $page
+* @var array $event
 */
 ?>
 <li class="eventsListItem">
   <?php
-    $date = strtotime($event['end'] ?? $event['start'] ?? '');
-?>
-  <time class="font-footnote mb-2"><?=date('d.m.Y, H:m', $date)?> Uhr</time>
-  <h3 class="font-subheadline mb-2"><?=$event['event']['name']?></h3>
+    $date = strtotime($event['start'] ?? '');
+  ?>
+  <a href="<?= "https://oveda.de/eventdate/" . ($event['id'] ?? '') ?>" target="_blank" rel="noopener noreferrer">
+  <time class="font-footnote mb-2"><?=date('d.m.Y, H:i', $date)?> Uhr</time>
+  <h3 class="font-subheadline mb-2"><?=Str::short($event['event']['name'], 50, '…') ?></h3>
   <p class="font-body mb-2"><?=Str::short($event['event']['description'], 120, '…')?></p>
+    </a>
+
 </li>
