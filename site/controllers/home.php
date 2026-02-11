b1254b1ef579c69eba5ade664c6f18ac01004c04
stuff <stuff@stuffdev.de>
2026-02-11 13:22:49 +0100

    oveda_events_fix

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -4,7 +4,7 @@ use Kirby\Http\Remote;
 
 return function () {
     //Fetches events from oveda for organization and from current date
-    $json = Remote::get('https://oveda.de/api/v1/event-dates/search?per_page=6&date_from='. date('Y-m-d') .'&organization_id=19')->json();
+    $json = Remote::get("https://oveda.de/api/v1/organizations/19/event-dates/search?per_page=6&event_list_id=9")->json();
     $events = $json['items'];
 
     //$events = [];
