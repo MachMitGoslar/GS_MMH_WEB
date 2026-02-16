3c7d8e496a28e0e0057d5dd681d4b410f8f1e0c5
Janssen, Nantke <nantke.janssen@goslar.de>
2026-02-16 15:50:57 +0100

    fix layout

--- site/templates/default.php
+++ site/templates/default.php
@@ -15,22 +15,25 @@
     <h1 class="font-titleXXL grid-item" data-span="1/1"><?=$page->title()?></h1>
 
     </div>
-    <?php foreach ($page->layout()->toLayouts() as $layout) : ?>
-      <div class="grid content">
+      <?php foreach ($page->layout()->toLayouts() as $layout) : ?>
+          <div class="grid content">
 
-        <?php foreach ($layout->columns() as $column) : ?>
-            <?php foreach ($column->blocks() as $block) : ?>
-        <div class="grid-item" data-span="<?=$column->width()?>">
-            <div id="<?= $block->id() ?>" class="c-blog c-blog-<?= $block->type() ?>">
-                <?= $block ?>
-            </div>
-        </div>
-            <?php endforeach ?>
+              <?php foreach ($layout->columns() as $column) : ?>
+                  <!-- DIESE DIV IST DIE SPALTE -->
+                  <div class="grid-item" data-span="<?= $column->width() ?>">
 
-        <?php endforeach ?>
-      </div>
+                      <?php foreach ($column->blocks() as $block) : ?>
+                          <div id="<?= $block->id() ?>" class="c-blog c-blog-<?= $block->type() ?>">
+                              <?= $block ?>
+                          </div>
+                      <?php endforeach ?>
 
-    <?php endforeach ?>
+                  </div>
+              <?php endforeach ?>
+
+          </div>
+      <?php endforeach ?>
+
   </section>
 
   </main>
