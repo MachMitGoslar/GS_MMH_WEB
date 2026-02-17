664bcd977cc6f90ed8cb6fe179e4172e09783dbb
stuff <stuff@stuffdev.de>
2026-02-17 14:47:18 +0100

    fixed date select field

--- site/controllers/events.php
+++ site/controllers/events.php
@@ -8,9 +8,9 @@ return function () {
 
     $json = "";
     if ($page = get('page')) {
-        $json = Remote::get("https://oveda.de/api/v1/organizations/19/event-dates/search?per_page=12&event_list_id=9&from=" . $today . "&page=".$page)->json();
+        $json = Remote::get("https://oveda.de/api/v1/organizations/19/event-dates/search?per_page=12&event_list_id=9&date_from=" . $today . "&page=".$page)->json();
     } else {
-        $json = Remote::get("https://oveda.de/api/v1/organizations/19/event-dates/search?per_page=12&event_list_id=9&from=" . $today)->json();
+        $json = Remote::get("https://oveda.de/api/v1/organizations/19/event-dates/search?per_page=12&event_list_id=9&date_from=" . $today)->json();
     }
     $events = $json['items'];
     //$events = [];
