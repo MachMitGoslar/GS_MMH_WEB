6658b04c49ed584d31a9839471da6bd2c1ea5ad8
Janssen, Nantke <nantke.janssen@goslar.de>
2026-02-17 13:23:43 +0100

    edit contact page

--- site/snippets/layout/head.php
+++ site/snippets/layout/head.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?=css('assets/css/index.css?version=2')?>
+    <link href="https://api.mapbox.com/mapbox-gl-js/v3.17.0/mapbox-gl.css" rel="stylesheet">
+    <script src="https://api.mapbox.com/mapbox-gl-js/v3.17.0/mapbox-gl.js"></script>
     <?= $slots->head() ?>
 </head>
 <body>
