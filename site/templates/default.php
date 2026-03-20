205a85a5d1acf3d5bc5d650911dd30445cdc2a89
Janssen, Nantke <nantke.janssen@goslar.de>
2026-03-20 11:59:35 +0100

    timemanagement tab in default page

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,41 +1,58 @@
 <?php
 /**
-* @var \Kirby\Cms\Site $site
-* @var \Kirby\Cms\Page $page
-*/
+ * @var \Kirby\Cms\Site $site
+ * @var \Kirby\Cms\Page $page
+ */
+
+// Eingeloggter User (Panel)
+$user = kirby()->user();
+
+// Zeitprüfung
+$now = time();
+$publish = $page->publish_date()->isNotEmpty() ? $page->publish_date()->toDate() : null;
+$end = $page->end_date()->isNotEmpty() ? $page->end_date()->toDate() : null;
+
+// Nur blockieren, wenn KEIN User eingeloggt ist
+if (!$user && (($publish && $publish > $now) || ($end && $end < $now))) {
+    go(site()->errorPage()->url(), 404);
+}
 ?>
+
 <?php snippet('layout/head'); ?>
 <?php snippet('layout/header'); ?>
-  <main>
-  <div class="mb-4">
-    <?=snippet('sections/hero')?>
-  </div>
-  <section class="">
-    <div class="grid content">
-    <h1 class="font-titleXXL grid-item" data-span="1/1"><?=$page->title()?></h1>
 
-    </div>
-      <?php foreach ($page->layout()->toLayouts() as $layout) : ?>
-          <div class="grid content">
+    <main>
+        <div class="mb-4">
+            <?= snippet('sections/hero') ?>
+        </div>
 
-              <?php foreach ($layout->columns() as $column) : ?>
-                  <!-- DIESE DIV IST DIE SPALTE -->
-                  <div class="grid-item" data-span="<?= $column->width() ?>">
+        <section>
+            <div class="grid content">
+                <h1 class="font-titleXXL grid-item" data-span="1/1">
+                    <?= $page->title() ?>
+                </h1>
+            </div>
 
-                      <?php foreach ($column->blocks() as $block) : ?>
-                          <div id="<?= $block->id() ?>" class="c-blog c-blog-<?= $block->type() ?>">
-                              <?= $block ?>
-                          </div>
-                      <?php endforeach ?>
+            <?php foreach ($page->layout()->toLayouts() as $layout) : ?>
+                <div class="grid content">
 
-                  </div>
-              <?php endforeach ?>
+                    <?php foreach ($layout->columns() as $column) : ?>
+                        <div class="grid-item" data-span="<?= $column->width() ?>">
 
-          </div>
-      <?php endforeach ?>
+                            <?php foreach ($column->blocks() as $block) : ?>
+                                <div id="<?= $block->id() ?>" class="c-blog c-blog-<?= $block->type() ?>">
+                                    <?= $block ?>
+                                </div>
+                            <?php endforeach ?>
 
-  </section>
+                        </div>
+                    <?php endforeach ?>
 
-  </main>
+                </div>
+            <?php endforeach ?>
+
+        </section>
+    </main>
+
 <?php snippet('layout/footer'); ?>
 <?php snippet('layout/foot'); ?>
